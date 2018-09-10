05e839f9d09c1408a6c7e2dac224da3200883886
Luke Winship <luke.winship@gmail.com>
2018-09-10 16:53:06 +0100

    Create gitbookScript.js

---
+++ search/v2/gitbookScript.js
@@ -0,0 +1,10 @@
+document.write(
+  '<style>\n' +
+  // Ensure Authorization modal doesn't render all the way down the page
+  '  .swagger-ui .dialog-ux .modal-ux {\n' +
+  '    top: 15rem;\n' +
+  '    width: 90%;\n' +
+  '  }\n' +
+  '</style>');
+document.write(
+  '<script src="//imin-ltd.github.io/swagger-ui-embed/search/v2/script.js"></script>');
