917c6279c1bcb405140ca5e64b032f409776b3f7
Luke Winship <luke.winship@gmail.com>
2018-09-10 16:55:24 +0100

    !important for style values to override swagger

--- search/v2/gitbookScript.js
+++ search/v2/gitbookScript.js
@@ -2,8 +2,8 @@ document.write(
   '<style>\n' +
   // Ensure Authorization modal doesn't render all the way down the page
   '  .swagger-ui .dialog-ux .modal-ux {\n' +
-  '    top: 15rem;\n' +
-  '    width: 90%;\n' +
+  '    top: 15rem !important;\n' +
+  '    width: 90% !important;\n' +
   '  }\n' +
   '</style>');
 document.write(
