01f04339bff883d3c7e7df8ffeb69886e38e6998
Luke Winship <luke.winship@gmail.com>
2018-09-10 15:13:47 +0100

    uses review app with better swaggs for now

--- search/v2/script.js
+++ search/v2/script.js
@@ -10,7 +10,8 @@ document.write(
   '<script>\n' +
   '  // Build a system\n' +
   '  var ui = window.SwaggerUIBundle({\n' +
-  '    url: "https://search.imin.co/v2/spec.yaml",\n' +
+  '    url: "https://imin-platform-search-te-pr-196.herokuapp.com/v2/spec.yaml",\n' +
+// '    url: "https://search.imin.co/v2/spec.yaml",\n' +
   '    dom_id: "#imin-swagger-ui-embed",\n' +
   '    presets: [\n' +
   '      SwaggerUIBundle.presets.apis,\n' +
