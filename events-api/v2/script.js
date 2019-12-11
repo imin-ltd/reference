735c3b3620eb4294856420937b80ff09ecf1b794
Luke Winship <luke.winship@gmail.com>
2019-12-11 14:31:16 +0000

    (search-sandbox -> search).imin.co

--- events-api/v2/script.js
+++ events-api/v2/script.js
@@ -10,7 +10,7 @@ document.write(
   '<script>\n' +
   '  // Build a system\n' +
   '  var ui = window.SwaggerUIBundle({\n' +
-  '    url: "https://search-sandbox.imin.co/events-api/v2/spec.yaml",\n' +
+  '    url: "https://search.imin.co/events-api/v2/spec.yaml",\n' +
   '    dom_id: "#imin-swagger-ui-embed",\n' +
   '    enableCORS: false,\n' +
   '    presets: [\n' +
